570467715d6f27ce6c2fdcc298ff7fa539b688a2
wei <miao87216932@me.com>
2021-09-01 23:06:21 +0100

    add R tutorial: R basics

--- build book.R
+++ build book.R
@@ -1,8 +1,9 @@
 library(bookdown)
 
+render_book( output_format = 'bookdown::pdf_book', config_file = '_bookdown.yml')
+
 render_book(config_file = '_bookdown.yml')
 
-render_book( output_format = 'bookdown::pdf_book', config_file = '_bookdown.yml')
 
 render_book( output_dir = "_book", config_file = '_bookdown.yml')
 
