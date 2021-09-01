e1235cd557b0a843f5515a411bd6dd1fad618c69
wei <miao87216932@me.com>
2021-09-01 12:20:05 +0100

    add pdf

--- build book.R
+++ build book.R
@@ -2,6 +2,8 @@ library(bookdown)
 
 render_book(config_file = '_bookdown.yml')
 
+render_book( output_format = 'bookdown::pdf_book', config_file = '_bookdown.yml')
+
 render_book( output_dir = "_book", config_file = '_bookdown.yml')
 
 render_book(clean = T,output_dir = "docs", rmd_subdir = T, delete_merged_file = T)
