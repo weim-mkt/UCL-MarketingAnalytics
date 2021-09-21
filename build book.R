4c9a93f26ddf3ba7106f245831f7e6ca0b3980b5
wei <miao87216932@me.com>
2021-09-21 16:47:41 +0100

    update pdf file accordingly.

--- build book.R
+++ build book.R
@@ -1,7 +1,7 @@
 library(bookdown)
 
-render_book( output_format = 'bookdown::pdf_book', config_file = '_bookdown.yml', )
+bookdown::render_book( output_format = 'bookdown::pdf_book', config_file = '_bookdown.yml', )
 
-render_book(config_file = '_bookdown.yml')
+bookdown::render_book(config_file = '_bookdown.yml')
 
 rmarkdown::render("RTutorial/IntroductionR.Rmd", output_format = 'slidy_presentation',  output_dir = '/Users/weimiao/Dropbox/UCL/Teaching/MSIN0094 Marketing Analytics/UCL 2021 - 2022/slides/Induction', output_yaml = "/Users/weimiao/Dropbox/UCL/Teaching/MSIN0094 Marketing Analytics/UCL 2021 - 2022/slides/Induction/_slidy.yaml",  clean = TRUE )
